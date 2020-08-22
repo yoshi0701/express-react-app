197d4dc21528e977abdccb4f3388de06a03aed97
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-08-23 01:48:46 +0900

    feat: client added

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+
+export default () => {
+  return (
+    <div className="container">
+      <h1>Creat Post</h1>
+      <PostCreate />
+      <hr/>
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+};
+
