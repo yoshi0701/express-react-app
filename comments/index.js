1580894aaedf8b880528b26186307949676c1d5e
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-08-22 16:53:17 +0900

    init

---
+++ comments/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors')
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostsId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostsId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostsId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostsId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+
+app.listen(4001, () => {
+  console.log('Listening on 4001')
+});
