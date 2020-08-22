ba522937f91717563e3467fd7ba49ba4dcade2ba
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-08-23 01:03:22 +0900

    fix: typo

--- comments/index.js
+++ comments/index.js
@@ -43,12 +43,12 @@ app.post('/events', async (req, res) => {
   const { type, data } = req.body;
 
   if (type === 'CommentModerated') {
-    const { postId, id, status } = data;
+    const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
+
     const comment = comments.find(comment => {
       return comment.id === id;
     });
-
     comment.status = status;
 
     await axios.post('http://localhost:4005/events', {
@@ -59,7 +59,7 @@ app.post('/events', async (req, res) => {
         postId,
         content
       }
-    })
+    });
   }
 
   res.send({});
